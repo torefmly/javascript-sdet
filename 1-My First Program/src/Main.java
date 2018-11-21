8c1b9f0ed99f68653f4b933850cd9b721eac50aa
tore <torefmly>
2018-11-21 13:28:37 -0500

    JavaProject

---
+++ 1-My First Program/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("I am the perfect guy!");
+		System.out.print("How are you");
+		System.out.println("I am good/Thanks");
+		System.out.println("Bye. Class is finished");
+	}
+
+}
